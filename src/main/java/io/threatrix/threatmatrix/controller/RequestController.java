10e0ff4e9cd3bb943fe084a56f17b8164c51347b
Aibek Osmonov <aosmonov@gmail.com>
2021-07-14 23:14:56 +0600

    Java files

--- src/main/java/io/threatrix/threatmatrix/controller/RequestController.java
+++ src/main/java/io/threatrix/threatmatrix/controller/RequestController.java
@@ -1,4 +1,18 @@
 package io.threatrix.threatmatrix.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping("/request")
 public class RequestController {
+
+    @Autowired
+    private RequestService requestService;
+
+    @GetMapping("/list")
+    public List<MyReqeust> getRequests(Principal principal) {
+        List<MyRequest> requests = requestService.getAllRequests();
+
+        return requests.filter(req => req.user == principal.getUser());
+    }
 }
